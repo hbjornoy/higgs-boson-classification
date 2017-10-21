5e553ecd70c663aec0b597e90ff4fa03c046f099
Thomas <thomas.ulvoen@gmail.com>
2017-10-21 12:42:39 +0200

    added covariance measurement and remove feature function

--- Python/cross_validation.py
+++ Python/cross_validation.py
@@ -1,4 +1,58 @@
 import numpy as np
+
+
+def cross_validation(function_to_run, y, x, num_of_k_fold, *args):
+    
+    losses = []
+    pred_acc_percents = []
+    
+    k_indices = build_k_indices(y, num_of_k_fold, 1)
+    
+    for k in range(num_of_k_fold):
+        
+        x_test, y_test, x_tr, y_tr = split_k(x,y,k_indices, k)
+        
+        if(function_to_run.__name__ == "reg_logistic_regression"):
+            
+            lambda_ = args[0]
+            initial_w = args[1]
+            max_iters = args[2]
+            gamma = args[3]
+            
+            loss, weights, losses =  function_to_run(y_tr, x_tr, lambda_, initial_w, max_iters, gamma) 
+            
+        elif(function_to_run.__name__ == "ridge_regression"):
+            
+            lambda_ = args[0]
+            
+            weights, loss = bf.ridge_regression(y, x, lambda_)          
+            
+            
+        losses.append(loss)
+
+        pred_y, soppel = bf.log_pred(x, weights)    
+        pred_acc_percent, soppel = bf.log_pred_acc(y, pred_y)
+        pred_acc_percents.append(pred_acc_percent)
+     
+    
+    loss_sum = 0
+    for loss in losses:
+        loss_sum += loss
+    avg_loss = loss_sum / len(losses)
+    
+    acc_sum = 0
+    for acc in pred_acc_percents:
+        acc_sum += acc
+    avg_acc = acc_sum / len(pred_acc_percents)
+    
+    return avg_loss, losses, avg_acc, pred_acc_percents 
+
+
+
+
+
+##### STUFF FROM OTHER SOURCES - PROB. NOT USED
+
 def build_k_indices(y, k_fold, seed):
     """build k indices for k-fold."""
     num_row = y.shape[0]
